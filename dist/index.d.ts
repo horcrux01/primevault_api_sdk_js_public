1c7f3f116caf41448893efc59af200597c215604
vivekkumar15 <kmr.vivek15@gmail.com>
2024-05-22 20:14:29 +0530

    expose types and constants

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,4 +1,6 @@
 export * from "./apiClient";
 export * from "./utils";
 export * from "./config";
+export * from "./constants";
+export * from "./types";
 //# sourceMappingURL=index.d.ts.map
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 export * from "./apiClient";
 export * from "./utils";
 export * from "./config";
+export * from "./constants";
+export * from "./types";

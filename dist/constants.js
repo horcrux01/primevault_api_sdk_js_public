d5352524bc490053019ae8db713702af87cf0d27
vivekkumar15 <kmr.vivek15@gmail.com>
2024-12-05 16:57:39 +0530

    build

--- dist/constants.js
+++ dist/constants.js
@@ -1,18 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.Chain = exports.SignatureServiceEnum = void 0;
+exports.SignatureServiceEnum = void 0;
 var SignatureServiceEnum;
 (function (SignatureServiceEnum) {
     SignatureServiceEnum["AWS_KMS"] = "AWS_KMS";
     SignatureServiceEnum["PRIVATE_KEY"] = "PRIVATE_KEY";
 })(SignatureServiceEnum || (exports.SignatureServiceEnum = SignatureServiceEnum = {}));
-var Chain;
-(function (Chain) {
-    Chain["ETHEREUM"] = "ETHEREUM";
-    Chain["POLYGON"] = "POLYGON";
-    Chain["SOLANA"] = "SOLANA";
-    Chain["NEAR"] = "NEAR";
-    Chain["APTOS"] = "APTOS";
-    Chain["ARBITRUM"] = "ARBITRUM";
-    Chain["OPTIMISM"] = "OPTIMISM";
-})(Chain || (exports.Chain = Chain = {}));

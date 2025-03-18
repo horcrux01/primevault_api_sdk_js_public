ffaf41fc7156ff06f509fe8484d0a31029424fa3
Vivek <42060544+vivekkumar15@users.noreply.github.com>
2025-03-18 22:11:04 +0530

    Update vault.tsx

--- examples/vault.tsx
+++ examples/vault.tsx
@@ -4,19 +4,7 @@ import {APIClient, BalanceResponse, Vault} from "../src";
 const createVault = async (apiClient: APIClient) => {
     const data = {
         "vaultName": "Ethereum Vault",                      // Vault name, should be unique
-        "defaultTransferSpendLimit": {                      // Default spend policy for transfer operations
-            "action": {                                     // action when the txn exceeds the current spend limit. Options are to ask for more approvals or block txn.
-                "actionType": "NEEDS_MORE_APPROVALS",
-                "additionalApprovalCount": 1
-            },
-            "spendLimit": "10000",                          // spend limit amount
-            "resetFrequency": "86400",                      // spend limit window, in seconds.
-        },
-        "defaultTradeSpendLimit": {                         // Default spend limit for trade operations
-            "action": {"actionType": "BLOCK_OPERATION"},
-            "spendLimit": "100",
-            "resetFrequency": "86400",
-        },
+        "templateId": "b188813e-3137-4b91-8534-f494cb198b8a" // Template to use for this vault
     }
 
     let vaultResponse: Vault = await apiClient.createVault(data);
